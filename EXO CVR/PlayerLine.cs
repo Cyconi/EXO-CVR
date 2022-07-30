89e8d57c87d1d7243585723c235caf37e7000964
Cyconi <jonhilde5@gmail.com>
2022-07-30 01:05:01 -0700

    uh idk

--- EXO CVR/PlayerLine.cs
+++ EXO CVR/PlayerLine.cs
@@ -55,7 +55,7 @@ namespace EXO_CVR
 
         internal static LineRenderer GetPlayerReady(CVRPlayerEntity Player)
         {
-            var LineRenderComp = Player.PlayerObject.GetOrAddComponents<LineRenderer>();
+            var LineRenderComp = Player.PlayerObject.GetOrAddComponents<LineRenderer>();            
             LineRenderComp.enabled = true;
             LineRenderComp.startWidth = 0.006f;
             LineRenderComp.alignment = 0;
